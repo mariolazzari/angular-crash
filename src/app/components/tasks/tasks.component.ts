5e5bc26a662cd1f8ff7082b34621caecaca7b2cf
Mario Lazzari <mario.lazzari@gmail.com>
2021-05-16 10:50:19 +0200

    Added task delete

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -16,7 +16,7 @@ export class TasksComponent implements OnInit {
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
-  deleteTak(task: Task) {
+  deleteTask(task: Task) {
     this.taskService
       .deleteTask(task)
       .subscribe(
